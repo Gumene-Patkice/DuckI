4f1c2edeaa957de79f713a3c91b751f77dacd55c
Jan Lalic <jan.lalic@fer.hr>
2024-11-10 18:08:33 +0100

    globalni eroor handeler middleware

---
+++ IzvorniKod/Helpers/GlobalErrorHandeler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+public class GlobalErrorHandeler
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalErrorHandeler(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context); // Call the next middleware
+        }
+        catch (Exception ex)
+        {
+            // Handle the exception and redirect as needed
+            context.Response.Redirect("/Home/UploadCalendar");
+        }
+    }
+}
