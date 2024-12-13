28dbc4c832f0939cc8d95ddd88b7ec967476ae56
Jan Badel <jan.badel@fer.unizg.hr>
2024-12-13 14:29:47 +0100

    Implementiran dizajn za Roles page, promjene na nekim stranicama Account Manage rute
    - vidljive Role u Profile
    - veći font za izbornik stranica Account Managea

---
+++ IzvorniKod/wwwroot/js/roles.js
@@ -0,0 +1,28 @@
+﻿document.getElementById("EducatorReset").addEventListener("click", function(){
+    document.getElementById("textarea1").value = "";
+    document.getElementById("EducatorReset").disabled = true;
+});
+
+document.getElementById("ReviewerReset").addEventListener("click", function(){
+    document.getElementById("textarea2").value = "";
+    document.getElementById("ReviewerReset").disabled = true;
+});
+
+function enableEducatorReset() {
+    if (document.getElementById("textarea1").value !== "") {
+        document.getElementById("EducatorReset").disabled = false;
+    } else {
+        document.getElementById("EducatorReset").disabled = true;
+    }
+}
+
+function enableReviewerReset() {
+    if (document.getElementById("textarea2").value !== "") {
+        document.getElementById("ReviewerReset").disabled = false;
+    } else {
+        document.getElementById("ReviewerReset").disabled = true;
+    }
+}
+
+document.getElementById("textarea1").addEventListener("input", enableEducatorReset);
+document.getElementById("textarea2").addEventListener("input", enableReviewerReset);
